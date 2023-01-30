51a17f175131a28f0bb36d231fabf165614c2862
danny6520 <dannychristofer2001@gmail.com>
2023-01-30 08:46:48 +0530

    second commit

--- db_connect/online_test_json_angular_db_connect.php
+++ db_connect/online_test_json_angular_db_connect.php
@@ -1,6 +1,6 @@
 <?php
-//$conn=new mysqli('localhost','root','','online_test_json_angular');
-$conn=new mysqli('localhost','admin','Admin@123#','pupsyothakadai');
+$conn=new mysqli('localhost','root','','online_test_json_angular');
+//$conn=new mysqli('localhost','admin','Admin@123#','pupsyothakadai');
 if($conn->connect_errno)
 {
 echo $conn->connect_error;
